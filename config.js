6070add4f24643b2f7200b9394f5ca5521d76013
Yuest Wang <btyuest@gmail.com>
2011-05-18 22:44:19 +0800

    git rm config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dir: {//dirs ending with '/', __dirname is the full path of config.js and '.' is the relative path to `pwd` of command line.
-        source: __dirname + "/tmd.js/tmd-source/"
-      , template: __dirname + "/tmd.js/tmd-templates/"
-      , output: __dirname + "/aaa"
-    }
-  , locals: {//default variables in templates
-    }
-};
