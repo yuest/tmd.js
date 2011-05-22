5000cbed7c7d80dd1a900424b3088d5d93dc0d73
Yuest Wang <btyuest@gmail.com>
2011-05-22 14:03:27 +0800

    rewrite renderFilesInDir

--- tmd.js
+++ tmd.js
@@ -8,163 +8,163 @@ var fs = require('fs')
   , quip = require('quip')
   , jade = require('jade')
   , tp = {}
-  , configDirPath = path.resolve('.')
-  , configDirLastTry
   , config
-  , builded
   ;
 
-while (configDirLastTry != configDirPath) {
-  try {
-    config = require(configDirPath + '/config');
-  } catch (e) {}
-  if (config) break;
-  configDirLastTry = configDirPath;
-  configDirPath = path.dirname(configDirPath);
-}
-if (!config) throw 'config.js not found';
+(function () {
+    var configDirPath = path.resolve('.')
+      , configDirLastTry
+      ;
+
+    while (configDirLastTry != configDirPath) {
+        try {
+            config = require(configDirPath + '/config');
+        } catch (e) {}
+        if (config) break;
+
+        configDirLastTry = configDirPath;
+        configDirPath = path.dirname(configDirPath);
+    }
+    if (!config) throw 'config.js not found';
+}());
 var srcDir = config.dir.source
   , tplDir = config.dir.template
   , outDir = config.dir.output
   ;
 
 ['document', 'listing'].forEach(function (name, ind, list) {
-  try {
-    tp[name] = jade.compile(fs.readFileSync(
-        path.join(config.dir.template, name + '.jade'), 'utf8'));
-  } catch (err) {
-    throw 'error on compile ' + path.join(config.dir.template, name + '.jade');
-  }
+    try {
+        tp[name] = jade.compile(fs.readFileSync(
+            path.join(config.dir.template, name + '.jade'), 'utf8'));
+    } catch (err) {
+        throw 'error on compile ' + path.join(config.dir.template, name + '.jade');
+    }
 });
 
-function renderJade(tpind, mdfilepath, fn) {
-  fs.readFile(mdfilepath, 'utf8', function (err, mdstr) {
-    if (err) return fn('not found');
-    fn(null, tp[tpind]({content: markdown.makeHtml(mdstr)}));
-  });
+function renderJade(tpind, mdfilepath, cb) {
+    fs.readFile(mdfilepath, 'utf8', function (err, mdstr) {
+        if (err) return cb('not found');
+        var html = markdown.makeHtml(mdstr)
+          , title = html.match(/<h1>(.*)<\/h1>/);
+          ;
+        title = title ? title[1] : path.basename( mdfilepath);
+        cb(null, tp[tpind]({ title: title, content: html}));
+    });
 }
 
 var router = function (app) {
-    /*
-  app.get('/*', function (req, res, next) {
-      if ( req.url[req.url.length - 1] == '/') return next();
-      if (!path.exists(path.join(config.dir.source, req.params[0] + '.md'))) {
-          res.redirect(req.url + '/');
-      }
-  });
-  */
-  app.get('/*', function (req, res, next) {
-    console.log(req.url);
-    var mdFilePath = path.join( srcDir, req.params[0] + '.md');
-    renderJade('document', mdFilePath, function (err, html) {
-      if (err) return res.notFound('notFound');
-      res.ok().html(html);
+    app.get('/*', function (req, res, next) {
+        var mdfilepath = path.join(srcDir, req.params[0] + '.md');
+        renderJade('document', mdfilepath, function (err, html) {
+            if (err) return next();
+            res.ok().html(html);
+        });
     });
-  });
-  /*
-  app.get('/*', function (req, res, next) {
-    var filepath = req.params[0];
-    if ('.html' != Array.prototype.splice.call(filepath, -5).join('')) {
-      filepath = filepath + '.html';
-    }
-    connectStatic.send(req, res, next, {
-        root: config.dir.output
-      , path: '/' + filepath
+    app.get('/*', function (req, res, next) {
+        var filepath = req.params[0];
+        if (! /index$/.test(filepath )) return next();
+        connectStatic.send(req, res, next, {
+            root: outDir
+            , path: '/' + filepath + '.html'
+        });
     });
-  });
-  */
-  app.get('/*', function (req, res, next) {
-    res.notFound('not found');
-  });
 };
 
 //dir - leading with '/' and not tailing with '/', for example '/static'
 //as - what directory you want your file to be served as, for example if it was set to '/public', request of '/public/file' will get './static/file' as a result. set to '', request of '/file' will get './static/file'
 var staticMiddleware = function (dir, as) {
-  if ( as[as.length - 1] == '/') as = as.substring(0, as.length - 1);
-  return function (req, res, next) {
-    var ind = req.url.indexOf(as)
-        , options = !ind
-                    ? {
-                        root: dir
-                      , path: req.url.substring(as.length)
-                    }
-                    : false
-        ;
-    if (!options) return next();
-    connectStatic.send(req, res, next, options);
-  };
+    if (as[as.length - 1] == '/') as = as.substring(0, as.length - 1);
+    return function (req, res, next) {
+        var ind = req.url.indexOf(as)
+            , options = !ind
+                        ? {
+                            root: dir
+                          , path: req.url.substring(as.length)
+                        }
+                        : false
+            ;
+        if (!options) return next();
+        connectStatic.send(req, res, next, options);
+    };
 };
 
 function updateUrl(req, res, next) {
     var tryDirectory = true
-      , filePath = path.join( srcDir, req.url );
-    if ( req.url[ req.url.length - 1 ] == '/') {
+      , filePath = path.join(srcDir, req.url );
+    if (req.url[ req.url.length - 1 ] == '/') {
         req.url = req.url + 'index';
     } else {
-        req.url = req.url.replace( /.html$/, function () {
+        req.url = req.url.replace(/.html$/, function () {
             tryDirectory = false;
             return '';
         });
-        if ( !path.exists( filePath + '.md')
+        if (!path.exists(filePath + '.md')
                 && tryDirectory
-                && path.exists( filePath ) && fs.statSync( filePath ).isDirectory()) {
-            req.redirect( req.url + '/');
+                && path.exists(filePath ) && fs.statSync(filePath ).isDirectory()) {
+            req.redirect(req.url + '/');
         }
     }
     next();
 }
 
-if (process.argv[2] in {'-d':1, '--dev':1}) {
-  connect(
-      quip()
-    , staticMiddleware(config.dir.staticFrom, config.dir.staticTo)
-    , updateUrl
-    , connect.router(router)
-  ).listen(3456);
-  console.log('development server started on 127.0.0.1:3456');
+function renderFilesInDir(dir) {
+    if (!dir) dir = srcDir;
+    fs.readdir(dir, function (err, files) {
+        if (err) throw 'error on reading source directory.';
+        var _dirs = []
+          , _mds = []
+          , _files = []
+          ;
+        files.forEach(function (file) {
+            if (fs.statSync(path.join(dir, file)).isDirectory()) {
+                _dirs.push(file);
+                renderFilesInDir(path.join(dir, file));
+            } else if ( fs.statSync(path.join(dir, file)).isFile()
+                       && /.md$/.test(file)) {
+                _mds.push(file);
+                renderFile(path.join(dir, file));
+            } else if (fs.statSync(path.join(dir, file)).isFile()) {
+                _files.push(file);
+            }
+        });
+        renderDir(dir, {
+            dirs: _dirs
+          , docs: _mds
+          , files: _files
+        });
+    });
+}
+function renderFile(srcFilePath) {
+    var outFilePath = srcFilePath.replace(srcDir, outDir).replace(/.md$/, '.html');
+    utils.mkdir_p( path.dirname( outFilePath));
+    renderJade('document', srcFilePath, function (err, html) {
+        fs.writeFile( outFilePath, html, function (err) {
+            if (err) console.log(err);
+        });
+    });
+}
+function renderDir(srcDirPath, locals) {
+    var outDirPath = srcDirPath.replace(srcDir, outDir);
+    utils.mkdir_p( outDirPath);
+    var html = tp.listing( locals);
+    fs.writeFile( outDirPath + '/tmd-listing.html', html, function (err) {
+        if (err) console.log(err);
+    });
+    if (!path.existsSync( srcDirPath + '/index.md')) {
+        fs.writeFile( outDirPath +'/index.html', html, function (err) {
+            if (err) console.log(err);
+        });
+    }
 }
 
-utils.fsfind(config.dir.source, /.md$/, function (err, file) {
-  if (err) throw err;
-  var outputFile = path.join(config.dir.output, 
-          file.substring(config.dir.source.length, file.length - 3) + '.html')
-    , outputFileDir = path.dirname(outputFile)
-    , _dirs = []
-    , _mds = []
-    , _files = []
-    ;
-  if (!path.existsSync(outputFileDir + '/tmd-listing.html')) {
-    fs.readdir(outputFileDir, function (err, files) {
-      var html, childhtmlname;
-      if (err) return console.log('error generating '+outputFileDir+'/tmd-listing.html');
-      files.forEach(function (child) {
-        if (fs.statSync(outputFileDir+'/'+child).isDirectory()) {
-          _dirs.push({href:child, title:child});
-        }
-        else if (fs.statSync(outputFileDir+'/'+child).isFile() && child.match(/.md$/)) {
-          childhtmlname = child.substring(0, child.lastIndexOf('.'))+'.html';
-          _mds.push({href:childhtmlname, title:childhtmlname});
-        }
-        else if (fs.statSync(outputFileDir+'/'+child).isFile()) {
-          _files.push({href:child, title:child});
-        }
-      });
-      html = tp.listing({docs:_mds, dirs:_dirs, files: _files});
-      fs.writeFile(path.dirname(outputFile)+'/tmd-listing.html', html, function (err) {
-        if (err) console.log(err);
-      });
-      if (!path.existsSync(path.dirname(file) + '/index.md')) {
-        fs.writeFile(path.dirname(outputFile)+'/index.html', html, function (err) {
-          if (err) console.log(err);
-        });
-      }
-    });
-  }
-  utils.mkdir_p(outputFileDir);
-  renderJade('document', file, function (err, html) {
-    fs.writeFile(outputFile, html, function (err) {
-      if (err) console.log(err);
-    });
-  });
-});
+if (process.argv[2] in {'-d':1, '--dev':1}) {
+    connect(
+        quip()
+      , staticMiddleware(config.dir.staticFrom, config.dir.staticTo)
+      , updateUrl
+      , connect.router(router)
+    ).listen(3456);
+    console.log('development server started on 127.0.0.1:3456');
+} else {
+    renderFilesInDir(srcDir);
+}
